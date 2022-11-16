3b98ab84124f7b054968eaad0fff1ec850d0d36d
MelroGui <melroguilherme3@gmail.com>
2022-11-16 00:34:08 -0300

    atual

--- back/src/controllers/VeiculosController.js
+++ back/src/controllers/VeiculosController.js
@@ -11,9 +11,9 @@ function listarVeiculos(req, res) {
     let query = "SELECT * FROM Veiculos";
 
     connect.query(query, (err, result) => {
-        if(err == null) {
+        if (err == null) {
             res.json(result).status(200).end();
-        }else {
+        } else {
             res.json(err).status(400).end();
         }
     })
@@ -23,9 +23,9 @@ function listaVeiculo(req, res) {
     let query = `SELECT * FROM Veiculos WHERE placa = '${req.params.placa}'`;
 
     connect.query(query, (err, result) => {
-        if(err == null) {
+        if (err == null) {
             res.status(200).json(result).end();
-        }else {
+        } else {
             res.status(400).json(err).end();
         }
     })
@@ -35,9 +35,9 @@ function cadastrarVeiculo(req, res) {
     let query = `INSERT INTO Veiculos VALUES ('${req.body.id_Cli}', '${req.body.placa}', '${req.body.cor}', '${req.body.modelo}', '${req.body.descricao}')`;
 
     connect.query(query, (err, result) => {
-        if(err == null) {
+        if (err == null) {
             res.status(201).json(req.body).end();
-        }else {
+        } else {
             res.status(400).json(err).end();
         }
     });
